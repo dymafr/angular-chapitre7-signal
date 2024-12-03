d5f2f3d645ecd363a42a16ea82af5b6604515198
quentin <quentin@dyma.fr>
2024-12-03 12:24:16 +0100

    feat: add code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { DymaComponent } from './components/dyma.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [DymaComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'dymacourse';
-}
+export class AppComponent {}
